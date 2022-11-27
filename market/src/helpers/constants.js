d09691fb39c4e336e77f3d5b9d64320fa00a5164
Mert Haddad <82902696+MertHaddad@users.noreply.github.com>
2022-11-27 17:04:59 +0300

    fix directory

--- market/src/helpers/constants.js
+++
@@ -1,2 +0,0 @@
-export const BRANDS_SERVICE_URL = "http://194.233.161.103:6001/companies";
-export const ITEMS_SERVICE_URL = "http://194.233.161.103:7000/items";
---
+++ src/helpers/constants.js
@@ -0,0 +1,2 @@
+export const BRANDS_SERVICE_URL = "https://194.233.161.103:6001/companies";
+export const ITEMS_SERVICE_URL = "https://194.233.161.103:7000/items";
